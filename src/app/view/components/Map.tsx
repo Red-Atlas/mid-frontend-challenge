c28dd046a25513e0701b638f8b41cc8de50e2c4f
nico-parco <nicolas.parco@taligent.com.ar>
2024-12-14 21:43:42 -0300

    feat: :sparkles: change marker

--- src/app/view/components/Map.tsx
+++ src/app/view/components/Map.tsx
@@ -1,25 +1,26 @@
-// import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css"; // Importa los estilos de Leaflet
 import { useEffect } from "react";
-import { MapContainer } from "react-leaflet/MapContainer";
-import { Marker } from "react-leaflet/Marker";
-import { Popup } from "react-leaflet/Popup";
-import { TileLayer } from "react-leaflet/TileLayer";
-import { useMap } from "react-leaflet/hooks";
+// import { MapContainer } from "react-leaflet/MapContainer";
+// import { Marker } from "react-leaflet/Marker";
+// import { Popup } from "react-leaflet/Popup";
+// import { TileLayer } from "react-leaflet/TileLayer";
+// import { useMap } from "react-leaflet/hooks";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
-import markerIconRetina from "leaflet/dist/images/marker-icon-2x.png";
+// import markerIconRetina from "leaflet/dist/images/marker-icon-2x.png";
 import L from "leaflet";
 
 interface MapProps {
   location: { lat: number; lng: number } | null; // Recibe location como prop
 }
 
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: markerIconRetina,
+const defaultIcon = L.icon({
   iconUrl: markerIcon,
   shadowUrl: markerShadow,
+  iconAnchor: [12, 41], // Ajusta el punto de anclaje del ícono
 });
+L.Marker.prototype.options.icon = defaultIcon; // Configura el ícono predeterminado para todos los marcadores
 function UpdateMapCenter({
   location,
 }: {
