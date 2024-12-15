c7bddbb20da197cba2c494cf145aa987101502b7
nico-parco <nicolas.parco@taligent.com.ar>
2024-12-14 21:10:23 -0300

    feat:

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["dummyimage.com", "fake-api-listings.vercel.app"], // Añadir el dominio aquí
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
