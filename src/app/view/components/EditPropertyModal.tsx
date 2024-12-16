46ebebeaba8c6ee761a20582c1c675f501571dbb
nico-parco <nicolas.parco@taligent.com.ar>
2024-12-16 03:06:57 -0300

    feat: :art: last fix responsive

--- src/app/view/components/EditPropertyModal.tsx
+++ src/app/view/components/EditPropertyModal.tsx
@@ -130,7 +130,7 @@ export default function EditPropertyModal({
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center">
-      <div className="bg-white w-1/3 p-6 rounded-lg shadow-lg relative">
+      <div className="bg-white w-full max-w-2xl p-6 rounded-lg shadow-lg relative overflow-y-auto max-h-[90vh] custom-scroll">
         <button
           onClick={onClose}
           className="absolute top-2 right-2 text-gray-500 hover:text-black"
